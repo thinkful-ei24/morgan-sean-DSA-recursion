ff1bbfdd7de234b41158ef141d7637c2d3b6c5e5
Sean Phelan <seanphelan97@gmail.com>
2018-11-05 14:52:12 -0700

    use memoization on fib problem

--- 8-fibonacci.js
+++ 8-fibonacci.js
@@ -1,7 +1,19 @@
 
 function fib(n) {
-  if(n < 3) return 1;
-  return fib(n-2) + fib(n-1);
+  const table = [0, 1, 1];
+  fib_memo(n);
+  function fib_memo(n) {
+    if(n < 3) return;
+  
+    if(!table[n-2]) {
+      fib_memo(n-2);
+    }
+    if(!table[n-1]) {
+      fib_memo(n-1);
+    }
+    table[n] = table[n-2] + table[n-1];
+  }
+  return table.join(' ');
 }
 
 console.log(fib(6)); // should print 8
